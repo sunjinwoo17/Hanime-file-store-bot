eb7810b383eaf9c80ba2004cc58ca57b39b6a95b
sunjinwoo17 <groottelegram@gmail.com>
2025-01-23 19:08:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from bot import Bot
 import pyrogram.utils
 
 # Set the minimum channel ID
-pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
+pyrogram.utils.MIN_CHANNEL_ID = -1002294126424
 
 # Run the bot
 if __name__ == "__main__":
